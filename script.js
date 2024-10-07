a964805ece65606fb11184f379eae7a20d53c58e
JSantarelli <santarellijulio@gmail.com>
2024-10-07 09:56:57 -0300

    feat(reponsive): select por default + indicadores

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
   window.onload = function() {
     const selectElement = document.getElementById("regionSelect");
-    selectElement.value = "Total Nacional (31 aglomerados urbanos)";
+    const selectElement2 = document.getElementById("customSelect");
+    selectElement.value = "Seleccione una ciudad o región";
+    selectElement2.value = "Seleccione una ciudad o región";
     actualizarBanderaPorRegion('regionSelect');
     actualizarBanderaPorRegion('customSelect');
-    // updateYear(2003);
-    
-    checkScreenWidth = () => {
-      
-      const screenWidth = window.innerWidth;
-     // const kpiWrapper = document.querySelector('.text__wrapper--grid'); // Sólo encontrará al primero
-      const kpiWrappers = document.querySelectorAll('.text__wrapper--grid'); // Sólo encontrará al primero
-
-      kpiWrappers.forEach(wrapper => {
-        if (screenWidth < 768) {
-          wrapper.classList.add('hidden');
-        } else {
-          wrapper.classList.remove('hidden');
-        }
-      })
-    }
-
-    checkScreenWidth()
-    window.addEventListener('resize', checkScreenWidth)
-    
   };
